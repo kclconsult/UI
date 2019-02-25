842d6655732074ba42d3b3d1fe3332cb216eb309
Martin Chapman <contact@martinchapman.co.uk>
2019-02-25 21:04:50 +0000

    Simplified application. Resolved data.

--- copy-shiny.sh
+++ copy-shiny.sh
@@ -1,7 +1,2 @@
-cp -R /home/kai/consult/* .
-git add .
-git commit -m "Update"
-pkill -f ssh-agent
-eval "$(ssh-agent -s)"
-ssh-add /home/martin/.ssh/consult-test-environment
-git push
+sudo cp -R . /srv/shiny-server/consult/
+sudo chown -R shiny:shiny /srv/shiny-server/consult/
--- test/app.R
+++
@@ -1,7 +0,0 @@
-library(plotly)
-
-x <- c(1:100)
-random_y <- rnorm(100, mean = 0)
-data <- data.frame(x, random_y)
-
-p <- plot_ly(data, x = ~x, y = ~random_y, type = 'scatter', mode = 'lines')
