3d9c6c3054fdf226e2e97bbc35fea0f09c73e321
0x02 <0x02@bu-con.earth>
2019-08-07 12:22:09 +0100

    navbar tab selection reported to server (for user logging)

--- dashboard/app/server.R
+++ dashboard/app/server.R
@@ -31,6 +31,42 @@ function(input, output) {
     # - ECG
     datasetECG = loadECGData(startTimestamp = "2016-02-26T00:00:00Z", endTimestamp = "2020-02-28T00:00:00Z")
     
+    
+    #
+    # Navbar Tab Changing Events for logging
+    # 
+    observeEvent(input$tabSummaryLink, {
+      print("Summary Tab Selected")
+    })
+    
+    observeEvent(input$tabHRLink, {
+      print("HR Tab Selected")
+    })
+    
+    observeEvent(input$tabBPLink, {
+      print("BP Tab Selected")
+    })
+    
+    observeEvent(input$tabECGLink, {
+      print("ECG Tab Selected")
+    })
+    
+    observeEvent(input$tabRiskLink, {
+      print("Risk Tab Selected")
+    })
+   
+    observeEvent(input$tabRecommendationsLink, {
+      print("Recommendations Tab Selected")
+    })
+    
+    observeEvent(input$tabMoodLink, {
+      print("Mood Tab Selected")
+    })
+
+    observeEvent(input$tabFeedbackLink, {
+      print("Feedback Tab Selected")
+    })
+    
     #
     # Tab: Summary
     #
@@ -145,113 +181,6 @@ function(input, output) {
     output$logFeedback = renderFeedback()
  
 }
-
-# From Sandbox...
-
-    # Variables defined in this function persist while the server is running.
-    # Reactivity can be defined either through using, observeEvent(), or 
-    # a reactive() block of code.
-    
-    # In the order that they are presented on the sandbox page:
-    
-    # Demonstrating Client-Server Interactivity
-
-    # Action button is clicked event handler
-    # times = 0 # track number of times clicked
-    # 
-    # observeEvent(input$actionBtn, {
-    #     print("actionBtn: Button Clicked")
-    # 
-    #     # <<- is the assignment operator in parent (i.e. global) environments
-    #     times <<- times + 1 
-    # 
-    #     # Render the Button Alert
-    #     output$buttonHTML <- renderButtonAlert(times)
-    # })
-    # 
-    # # ---
-    # 
-    # # Slider value is changed event handler
-    # observeEvent(input$sliderX, {
-    #     print("sliderX")
-    #     print(input$sliderX)
-    # 
-    #     # Render the Progress Bar
-    #     output$sliderProgressHTML <- renderSliderProgress(input$sliderX)
-    # })
-    # 
-    # # Demonstrate Text Input->Output
-    # 
-    # # Textarea
-    # observeEvent(input$exampleText, {
-    #     print("exampleText")
-    #     print(input$exampleText)
-    #     
-    #     # Render the Input Text as Reccomendation Text
-    #     output$recommendationsText <- renderText(input$exampleText)
-    # })
-    # 
-    # # Demonstrate HTML Widget (C3 Gauge)
-    # 
-    # # reactive expression that generates a random value for the gauge
-    # # https://www.rdocumentation.org/packages/shiny/versions/1.3.2/topics/reactive
-    # gaugeValue = reactive({
-    #     input$updateBtn # only exists in the expression to make it react the input event
-    #     round(runif(1,0,100), 2)
-    # })
-    # 
-    # output$gauge1 <- renderC3Gauge({
-    #     # C3Gauge widget
-    #     C3Gauge(gaugeValue())
-    # })
-    # 
-    # # Demonstrate HTML Widget (C3 Timeline) with Random Data
-    # 
-    # # reactive expression for the data
-    # timelineData = reactive({
-    #     input$refreshDataButton # only exists in the expression to make it react the input event
-    # 
-    #     # Example Timeline Data ('Time' -> x-axis, any other key's are plotted as lines on y-axis):
-    #     # timelineData = data.frame(Time = c("2016-01-01","2016-01-02","2016-01-03"), A=c(1,4,7), B=c(2,5,9))
-    #     
-    #     # Verify the data by converting to JSON, does this match what the chart expects?
-    #     # toJSON(timelineData)
-    #     # [{"Time":"2016-01-01","A":1,"B":2},{"Time":"2016-01-02","A":4,"B":5},{"Time":"2016-01-03","A":7,"B":9}] 
-    #     
-    #     # Random Values
-    #     data.frame(Time = c("2016-01-01","2016-01-02","2016-01-03", "2016-01-04","2016-01-05","2016-01-06"),
-    #                       A=round(runif(6,0,100), 2),
-    #                       B=round(runif(6,0,100), 2),
-    #                       C=round(runif(6,0,100), 2))
-    # })
-    # 
-    # output$timeline1 <- renderC3Timeline({
-    #     C3Timeline(dataset = timelineData())
-    # })
-    # 
-    # # Demonstrate Sample Data
-    # sampleData <- loadSampleTimelineData()
-    # step = 1
-    # windowSize = 7
-    # 
-    # # reactive expression for the data
-    # sampleTimelineData = reactive({
-    #     input$moreDataButton
-    #     windowSize <<- windowSize + 1 
-    # 
-    #     # Show a windowSize data frame at a specific step
-    #     #data.frame(Time       = as.vector(sampleData$datem[step:(step+windowSize)]),
-    #     #           c27164900  = as.vector(sampleData$c271649006[step:(step+windowSize)]),
-    #     #           c271650006 = as.vector(sampleData$c271650006[step:(step+windowSize)]),
-    #     #           c8867h4    = as.vector(sampleData$c8867h4[step:(step+windowSize)]))
-    # 
-    #     # return a slice of data the size of windowSize:
-    #     sampleData[step:(step+windowSize),]
-    # })
-    # 
-    # output$timeline2 <- renderC3Timeline({
-    #     C3Timeline(dataset = sampleTimelineData())
-    # })
 
 
 
--- dashboard/app/ui.R
+++ dashboard/app/ui.R
@@ -20,6 +20,25 @@ library(Consult)
 # See https://shiny.rstudio.com/articles/templates.html for more info.
 
 htmlTemplate("www/index.html",
+  # NavBar Tab Buttons (actionLinks allow for Shinky reactivity)
+  # TODO -  icon = NULL, ...)
+  # <a href="#summary" data-toggle="tab">Summary</a>
+  tabSummaryLink = actionLink("tabSummaryLink", "Summary", href = "#summary", `data-toggle` = "tab"),
+  # <a href="#hr" data-toggle="tab">Heart Rate</a>
+  tabHRLink = actionLink("tabHRLink", "Heart Rate", href = "#hr", `data-toggle` = "tab"),
+  # <a href="#bp" data-toggle="tab">Blood Pressure</a>
+  tabBPLink = actionLink("tabBPLink", "Blood Pressure", href = "#bp", `data-toggle` = "tab"),
+  # <a href="#ecg" data-toggle="tab">ECG</a>
+  tabECGLink = actionLink("tabECGLink", "ECG", href = "#ecg", `data-toggle` = "tab"),
+  # <a href="#risk" data-toggle="tab">Risk</a>
+  tabRiskLink = actionLink("tabRiskLink", "Risk", href = "#risk", `data-toggle` = "tab"),
+  # <a href="#recommendations" data-toggle="tab">Recommendations</a>
+  tabRecommendationsLink = actionLink("tabRecommendationsLink", "Recommendations", href = "#recommendations", `data-toggle` = "tab"),
+  # <a href="#mood" data-toggle="tab">Mood</a>
+  tabMoodLink = actionLink("tabMoodLink", "Mood", href = "#mood", `data-toggle` = "tab"),
+  # <a href="#feedback" data-toggle="tab">Feedback</a>
+  tabFeedbackLink = actionLink("tabFeedbackLink", "Feedback", href = "#feedback", `data-toggle` = "tab"),
+  
   # Tab: Summary
   # - time range selectors (TODO change from actionButtons to tabbed selector)
   selectLastFourHours = actionButton("selectLastFourHours", "Last 4 Hours"),
