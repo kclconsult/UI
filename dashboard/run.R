cdc344c6c9b52400dca6376d409f34e64cee0f3a
0x02 <0x02@bu-con.earth>
2019-08-08 14:40:06 +0100

    development run script

---
+++ dashboard/run.R
@@ -0,0 +1,15 @@
+# Development environment variables
+Sys.setenv(MESSAGE_PASSER_PROTOCOL="http://", MESSAGE_PASSER_URL="ec2-3-9-227-22.eu-west-2.compute.amazonaws.com:3005", SHINYPROXY_USERNAME="3e2dab80-b847-11e9-8e30-f5388ac63e8b", CURL_CA_BUNDLE="")
+
+library(shiny)
+
+# install packages
+library(devtools)
+setwd("dashboard/packages/Consult")
+devtools::install()
+setwd("../..")
+
+# runs app/ directory
+runApp(appDir="app", host="0.0.0.0", port=5369)
+
+
