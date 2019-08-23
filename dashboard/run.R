eddb0446a4813a25cbb1194a644eb39ae3d17a91
0x02 <0x02@bu-con.earth>
2019-08-23 10:23:37 +0100

    reverting run.R back to server set-up

--- dashboard/run.R
+++ dashboard/run.R
@@ -1,13 +1,16 @@
 # Development environment variables
-Sys.setenv(MESSAGE_PASSER_PROTOCOL="http://", MESSAGE_PASSER_URL="ec2-3-9-227-22.eu-west-2.compute.amazonaws.com:3005", SHINYPROXY_USERNAME="3e2dab80-b847-11e9-8e30-f5388ac63e8b", CURL_CA_BUNDLE="")
+Sys.setenv(MESSAGE_PASSER_PROTOCOL="http://", 
+           MESSAGE_PASSER_URL="ec2-3-9-227-22.eu-west-2.compute.amazonaws.com:3005", 
+           SHINYPROXY_USERNAME="3e2dab80-b847-11e9-8e30-f5388ac63e8b", 
+           CURL_CA_BUNDLE="")
 
 library('shiny')
 
 # install packages
 library('devtools')
-setwd('/Users/sklar/research/consult/dev/ui-reconstruction/dashboard/packages/Consult')
+setwd('dashboard/packages/Consult')
 devtools::install()
-setwd('/Users/sklar/research/consult/dev/ui-reconstruction/dashboard')
+setwd('../..')
 
 # runs app/ directory
 runApp(appDir="app", host="0.0.0.0", port=5369)
