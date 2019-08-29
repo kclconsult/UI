8f67d0eea3e95510ce65f1d7aa7f063506b4c565
chipp <chipp.jansen@kcl.ac.uk>
2019-08-29 20:13:09 +0100

    release it

--- dashboard/app/version.R
+++ dashboard/app/version.R
@@ -1,3 +1,3 @@
 # Version of the Dashboard App to be displayed in the UI (and server)
-DASHBOARD_VERSION = "0.0.38"
+DASHBOARD_VERSION = "0.1.0"
 print(paste("Consult Shiny Dashboard UI v", DASHBOARD_VERSION))
